7886872c31c93d66cc018ac53cc139299e2fde6a
Urooj Baksh <72161323+UroojBaksh@users.noreply.github.com>
2023-11-05 20:39:32 +0500

    Last commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -30,7 +33,7 @@ app.get('/', (req, res) => {
     res.send('Ngrok is working and can reach the Express server!');
   });
 
-const PORT = process.env.PORT || 443 ;
+const PORT = process.env.PORT || 3001 ;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
